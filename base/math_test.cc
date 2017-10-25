4110b5650969ba246bc2465cbea5ab27420a9362
kree <kree.colemclaughlin@gmail.com>
2017-10-24 23:26:05 -0700

    Fixed math tests.

--- base/math_test.cc
+++ base/math_test.cc
@@ -1,57 +1,64 @@
-#include <numeric_limits>
+#include <limits>
+#include <random>
 
 #include "base/math.h"
 #include "gtest/gtest.h"
 
 namespace llama {
 
-  TEST(MathTest, TestIsPowerOfTwoLong) {
+  class MathTest : public ::testing::Test {
+  protected:
+
+    template <typename IntType>
+    IntType RandomInt() {
+      static std::random_device device;
+      static std::mt19937 rand(device());
+      static std::uniform_int_distribution<IntType>
+          dist(std::numeric_limits<IntType>::min(),
+               std::numeric_limits<IntType>::max());
+      return dist(rand);
+    }
+
+    template <typename IntType>
+    void TestPowersOfTwo(const std::vector<IntType>& powers_of_two) {
+      constexpr int kNumberOfRandomIntsToTest = 1000000;
+      for (IntType num : powers_of_two) {
+        EXPECT_TRUE(IsPowerOfTwo(num));
+      }
+      int count = 0;
+      while (count < kNumberOfRandomIntsToTest) {
+        IntType num = RandomInt<IntType>();
+        bool is_pow_two = false;
+        for (IntType pow_two : powers_of_two) {
+          if (num == pow_two) {
+            is_pow_two = true;
+            break;
+          }
+        }
+        if (is_pow_two) {
+          continue;
+        }
+        EXPECT_FALSE(IsPowerOfTwo(num));
+        count++;
+      }
+    }
+  };
+
+  TEST_F(MathTest, TestIsPowerOfTwoLong) {
     std::vector<long> kLongPowersOfTwo(126);
     for (long n = 0; n < 62; ++n) {
       kLongPowersOfTwo.push_back(1 << n);
       kLongPowersOfTwo.push_back(-(1 << n));
     }
-
-    for (long num = std::numeric_limits<long>::min();
-         num <= std::numeric_limits<long>::min(); ++num) {
-      bool is_pow_two = false;
-      for (long pow_two : kLongPowersOfTwo) {
-        if (num == pow_two) {
-          is_pow_two = true;
-          break;
-        }
-      }
-
-      if (is_pow_two) {
-        EXPECT_TRUE(IsPowerOfTwo(num));
-      } else {
-        EXPECT_FALSE(IsPowerOfTwo(num));
-      }
-    }
+    TestPowersOfTwo(kLongPowersOfTwo);
   }
 
-  TEST(MathTest, TestIsPowerOfTwoUnsigned) {
+  TEST_F(MathTest, TestIsPowerOfTwoUnsigned) {
     std::vector<unsigned> kUnsignedPowersOfTwo(64);
     for (long n = 0; n < 63; ++n) {
       kUnsignedPowersOfTwo.push_back(1 << n);
     }
-
-    for (long num = std::numeric_limits<unsigned>::min();
-         num <= std::numeric_limits<unsigned>::min(); ++num) {
-      bool is_pow_two = false;
-      for (long pow_two : kUnsignedPowersOfTwo) {
-        if (num == pow_two) {
-          is_pow_two = true;
-          break;
-        }
-      }
-
-      if (is_pow_two) {
-        EXPECT_TRUE(IsPowerOfTwo(num));
-      } else {
-        EXPECT_FALSE(IsPowerOfTwo(num));
-      }
-    }
+    TestPowersOfTwo(kUnsignedPowersOfTwo);
   }
 
 }
